fedf046298db4fad0f1d3fa5c523254c03d2e362
josie <josiepjam@gmail.com>
2026-03-30 15:06:59 +0100

    db update

--- scran_users.sql
+++ scran_users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2026 at 05:37 PM
+-- Generation Time: Mar 30, 2026 at 04:06 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -45,6 +45,29 @@ INSERT INTO `contact_messages` (`id`, `name`, `email`, `message`, `created_at`) 
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `meal_id` varchar(20) NOT NULL,
+  `comment` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`id`, `user_id`, `meal_id`, `comment`, `created_at`) VALUES
+(3, 4, '52842', 'i enjoyed this', '2026-03-30 13:40:01'),
+(8, 3, '52842', 'good soup', '2026-03-30 14:04:29'),
+(9, 3, '53076', 'These instructions are silly!', '2026-03-30 14:05:35');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `saved_recipes`
 --
 
@@ -61,7 +84,9 @@ CREATE TABLE `saved_recipes` (
 
 INSERT INTO `saved_recipes` (`id`, `user_id`, `meal_id`, `created_at`) VALUES
 (111, 3, '53381', '2026-03-27 15:37:29'),
-(113, 3, '53099', '2026-03-27 19:57:29');
+(113, 3, '53099', '2026-03-27 19:57:29'),
+(114, 3, '53220', '2026-03-30 10:28:35'),
+(116, 3, '52965', '2026-03-30 10:30:49');
 
 -- --------------------------------------------------------
 
@@ -82,7 +107,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
-(3, 'Josie Pollard', 'josie@g.com', '$2y$10$hTa8uLXEygYGiiOJkNgyuuG8XiikPbbarab6Q56A0hG0VIJKlOm5G', '2026-03-21 21:04:18');
+(3, 'Josie', 'josie@g.com', '$2y$10$hTa8uLXEygYGiiOJkNgyuuG8XiikPbbarab6Q56A0hG0VIJKlOm5G', '2026-03-21 21:04:18'),
+(4, 'hello', 'hi@hello.com', '$2y$10$q7YEpmKluWEqXl6ZFhreAOygTgnI8708p9NQefElC5g3BgaWstcGe', '2026-03-30 13:39:39');
 
 --
 -- Indexes for dumped tables
@@ -93,6 +119,13 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
 --
 ALTER TABLE `contact_messages`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`,`meal_id`);
 
 --
 -- Indexes for table `saved_recipes`
@@ -119,20 +152,32 @@ ALTER TABLE `contact_messages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `reviews`
+--
+ALTER TABLE `reviews`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `saved_recipes`
 --
 ALTER TABLE `saved_recipes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=114;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `reviews`
+--
+ALTER TABLE `reviews`
+  ADD CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `saved_recipes`
